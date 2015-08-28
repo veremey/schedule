5ab9907f6cee0faf90f4a7c410201125ba3b245e
veremey <veremey@i.ua>
2015-08-28 14:30:59 +0300

    2

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-head.ready(function() {
+$(document).ready(function() {
 
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
@@ -14,6 +14,40 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
+		// if($(document).scrollTop()) {
+	 //            $(".main__img").removeClass('blur');
+	 //    } else{
+	 //    	$(".main__img").addClass('blur');
+	 //    };
+
+
+
+
+	 $('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 800,
+        onScroll: function(scrollPos){
+            // scrollPos:Number
+            // scrollPos: 1,
+            // $('.main__img').addClass('blur');
+        },
+         onSnapComplete: function($el){
+             // after window ($el) snaps into place
+        //    	$('.main__img').addClass('blur')
+        },
+        onWindowEnter: function($el){
+            // when new window ($el) enters viewport
+            // $('.main__img').removeClass('blur');
+        }
+    });
+
+
+	// $('.window:eq(1)').parents().find('.main__img').css({'blur':'5px', '-webkit-blur':'5px'});
+	// } else{
+	// 	$(this).parent().find('.main__img').addClass('blur');
+	// };
+
 
 	console.log($('body').html());
 });
