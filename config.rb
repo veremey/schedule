718e70a153bd276ebfbb65e5b5d870c4ad8ede8c
veremey <veremey@i.ua>
2015-08-31 10:22:37 +0300

    send

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compact
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
@@ -48,6 +48,6 @@ preferred_syntax = :sass
 #     assert_type string, :String
 #     Sass::Script::String.new("../img/#{string.value}")
 #   end
-#   alias_method :"image-path",:image_path 
+#   alias_method :"image-path",:image_path
 #   declare :"image-path", :args => [:string]
 # end
--- js/common.js
+++ js/common.js
@@ -9,33 +9,47 @@ $(document).ready(function() {
 	$('.form_1').on('click', function() {
 		$(this).siblings().removeClass('is_active');
 		$(this).addClass('is_active');
-		$('#form_1').siblings(":not(a)").fadeOut('fast');
-		$('#form_1').fadeIn('fast');
+		$('#form_1').siblings().removeClass('is_active');
+		$('#form_1').addClass('is_active');
 	});
 	$('.form_2').on('click', function() {
 		$(this).siblings().removeClass('is_active');
 		$(this).addClass('is_active');
-		$('#form_2').siblings(":not(a)").fadeOut('fast');
-		$('#form_2').fadeIn('fast');
+		$('#form_2').siblings(":not(a)").removeClass('is_active');
+		$('#form_2').addClass('is_active');
 	});
 	$('.form_3').on('click', function() {
 		$(this).siblings().removeClass('is_active');
 		$(this).addClass('is_active');
-		$('#form_3').siblings(":not(a)").fadeOut('fast');
-		$('#form_3').fadeIn('fast');
+		$('#form_3').siblings(":not(a)").removeClass('is_active');
+		$('#form_3').addClass('is_active');
 	});
 
 	if($('.form_1').hasClass('is_active')){
-		$('#form_1').fadeIn('fast');
+		$('#form_1').addClass('is_active');
 	};
 	if($('.form_2').hasClass('is_active')){
-		$('#form_2').fadeIn('fast');
+		$('#form_2').addClass('is_active');
 	};
 	if($('.form_3').hasClass('is_active')){
-		$('#form_3').fadeIn('fast');
+		$('#form_3').addClass('is_active');
 	};
 
+	//slider
+	if($(document).width() < 768){
+		$('.task__wrap').slick({
+			dots: true,
+			infinite: false,
+			speed: 900,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			adaptiveHeight: true
+		});
+	};
 
-
+	$('.btn__hiden-767').on('click',function() {
+		$('.header').toggleClass('header_100');
+		$('.header__menu li:not(.is_active), .socio').toggleClass('is_act');
+	});
 
 });
